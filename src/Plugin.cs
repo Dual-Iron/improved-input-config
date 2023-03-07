9841356b0402e7dab41031358585df95bf94ef11
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2023-03-06 22:26:49 -0500

    Start mod

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -6,28 +6,12 @@ using System.Security.Permissions;
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
 
-namespace TestMod;
+namespace BetterInputConfig;
 
-[BepInPlugin("com.author.testmod", "Test Mod", "0.1.0")]
+[BepInPlugin("com.dual.better-input-config", "Better Input Config", "1.0.0")]
 sealed class Plugin : BaseUnityPlugin
 {
-    bool init;
-
     public void OnEnable()
     {
-        // Add hooks here
-        On.RainWorld.OnModsInit += OnModsInit;
-    }
-
-    private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
-    {
-        orig(self);
-
-        if (init) return;
-
-        init = true;
-
-        // Initialize assets, your mod config, and anything that uses RainWorld here
-        Logger.LogDebug("Hello world!");
     }
 }
